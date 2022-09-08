e2b3e62184ae3e9e8433b955d16e85eab7feaf9f
Andrew <andrew_lowe1@baylor.edu>
2022-09-08 11:44:01 -0700

    worked on start file

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,26 +10,33 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook))
+print(phonebook['Chris'])
+phone = phonebook['Chris']
 
+print(phone)
 
+mydictionary = {}
+print(mydictionary)
 
+mydictionary = dict(m=8,n=9) 
 
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
-
-
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'Chris'
 
-
-
-
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(name, "is not in the phonebook")
 
 
 print()
@@ -37,17 +44,14 @@ print('*****  end section 2 ********')
 print()
 
 
-
-
-
-
-
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
-
-
+print(phonebook)
+phonebook['Chris'] = '555-0123'
+phonebook['Joe'] = '555-4444'
+print(phonebook)
 
 
 print()
@@ -56,56 +60,56 @@ print()
 
 
 
-
-
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
+#del phonebook['Chris']
+#print(phonebook)
 
 
 print()
 print('*****  end section 4 ********')
 print()
 
-
-
-
-
-
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+for key in phonebook:
+    print(key)
+    print(phonebook[key])
 
+for value in phonebook.values():
+    print(value)
 
+for k,v in phonebook.items():
+    print("key: ",k, " value:" ,v)
 
-
+for tuple in phonebook.items():
+    print(tuple)
 
 print()
 print('*****  end section 5 ********')
 print()
 
 
-
-
-
 print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+phone = phonebook.get("Chris","key not found")
+print(phone)
 
-
-
+#phonebook.clear()
+#print(phonebook)
 
 
 print()
 print('*****  end section 6 ********')
 print()
 
-
+...
 
 print()
 print('*****  start section 7 - using pop method ********')
